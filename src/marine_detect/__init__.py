bd1dca5d25a10b92600364e7730cc60f126da457
eleonorecharles <eleonore.charles@bd-orange.com>
2024-02-18 11:35:13 +0000

    initial commit
    Signed-off-by: eleonorecharles <eleonore.charles@bd-orange.com>

---
+++ src/marine_detect/__init__.py
@@ -0,0 +1,14 @@
+"""
+Init.
+
+Software Name : marine-detect
+SPDX-FileCopyrightText: Copyright (c) Orange Business Services SA
+SPDX-License-Identifier: AGPL-3.0-only.
+
+This software is distributed under the GNU Affero General Public License v3.0,
+the text of which is available at https://spdx.org/licenses/AGPL-3.0-only.html <https://spdx.org/licenses/AGPL-3.0-only.html>
+or see the "LICENSE" file for more details.
+
+Authors: Eléonore Charles
+Software description: Object detection models for identifying species in marine environments.
+"""
